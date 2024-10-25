7468fc36536af39937c92b38d6530b7046e5c188
ishaniff <idhaniff@hotmail.com>
2024-10-24 22:54:02 -0400

    html css js touchup, inserting content

--- questions/faq.js
+++ questions/faq.js
@@ -3,6 +3,11 @@ const currentYear = new Date().getFullYear();
 // Event Listener that runs when DOM content is loaded
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".sidenav");
+  var elem = document.querySelector(".collapsible.expandable");
+  var instance = M.Collapsible.init(elem, {
+    accordion: true,
+  });
+
   // Scans for document with any element with .sidenav, render it on the right side.
   var instances = M.Sidenav.init(elems, { edge: "right" });
 });

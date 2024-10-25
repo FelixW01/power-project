d8bdaa9875c613edee3656631c1916b0d9f4879e
Shifty3 <izzym407@gmail.com>
2024-10-25 11:46:02 -0400

    changed javascript

--- aboutUs/aboutUsJs.js
+++ aboutUs/aboutUsJs.js
@@ -7,14 +7,3 @@ document.addEventListener("DOMContentLoaded", function () {
   var instances = M.Sidenav.init(elems, { edge: "right" });
 });
 
-// Get the current year to create a dynamic footer
-document.getElementById("current-year").textContent = currentYear;
-
-document.addEventListener("DOMContentLoaded", function () {
-  var elems = document.querySelectorAll(".dropdown-trigger");
-  var instances = M.Dropdown.init(elems, options);
-});
-
-// Or with jQuery
-
-$(".dropdown-trigger").dropdown();

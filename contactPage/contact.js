89f9a9e5c17362d2e43ab507070215f12a4faf11
efr-en <efrenlaris@gmail.com>
2024-10-25 17:52:47 -0400

    added contact hero image, heading with org info, and css for form. Still not responsive

--- contactPage/contact.js
+++ contactPage/contact.js
@@ -1,52 +1,62 @@
 /* Appearing comment box*/
 function toggleCommentBox() {
-  const selectElement = document.getElementById('options-form');
-  const commentBox = document.getElementById('commentBox');
-  if (selectElement.value !== 'default') {
-    commentBox.style.display = 'block';
-  } else {
-    commentBox.style.display = 'none';
+  const selectElement = document.getElementById("options-form");
+  const commentBox = document.getElementById("commentBox");
+  // commentBox.style.display = (selectElement.value !== "default") ? "block" : "none";
+      if (selectElement.value !== "default") {
+        commentBox.style.display = 'block';
+      } else {
+        commentBox.style.display = 'none';
   }
-}
+};
 
-/* Form validation */
+
+/* Form validation */ 
+/*
 function validateForm() {
-  document.querySelectorAll('error').forEach(el => el.classList.add('hidden'))
+  document.querySelectorAll('.error').forEach(el => el.classList.add('hidden'))
   let isValid = true;
   const firstName = document.getElementById("firstName").value.trim();
   if (firstName === "") {
     document.getElementById("firstNameError").classList.remove('hidden');
     isValid = false;
   }
+
   const lastName = document.getElementById("lastName").value.trim();
   if (lastName === "") {
     document.getElementById("lastNameError").classList.remove('hidden');
     isValid = false;
   }
+
   const email = document.getElementById("email").value.trim();
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (email === "" || !emailRegex.test(email)) {
     document.getElementById("emailError").classList.remove('hidden');
     isValid = false;
   }
+
   const phone = document.getElementById("phone").value.trim();
   const phoneRegex = /^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[\s.-]?)?\d{3}[\s.-]?\d{4}$/;
   if (phone === "" || !phoneRegex.test(phone)) {
     document.getElementById("phoneError").classList.remove('hidden');
     isValid = false;
   }
-  const options = document.getElementById("options").value;
+  
+  const options = document.getElementById("options-form").value;
   if (options === "default") {
     document.getElementById("optionsError").classList.remove('hidden');
   }
   return isValid;
+  
 }
 
-document.getElementById('').addEventListener('submit', function(event)) {
+document.getElementById('contactForm').addEventListener('submit', function(event)) {
   if (!validateForm()) {
-    e.preventDefault();
+    event.preventDefault();
   }
-};
+}; */
+
+
 
 /* Dynamic footer and hamburger nav bar code*/
 const currentYear = new Date().getFullYear();

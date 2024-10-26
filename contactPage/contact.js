6d8dd19748df54c7cf2484900d3061910010d423
efr-en <efrenlaris@gmail.com>
2024-10-26 18:07:14 -0400

    added language translator onto contact page

--- contactPage/contact.js
+++ contactPage/contact.js
@@ -15,6 +15,21 @@ function googleTranslateElementInit() {
   new google.translate.TranslateElement({pageLanguage: 'en'},'google_translate_element');
 } 
 
+// Event Listener that runs when DOM content is loaded (This is from materializecss)
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  const languageEl = document.getElementById('language-button');
+  // Scans for document with any element with .sidenav, render it on the right side.
+  // Create an array of instances, this is created by the .init method from materializecss
+  var instances = M.Sidenav.init(elems, {edge: 'right'});
+
+  // To open sidenav when user doesn't have access to the hamburger menu, listen to a click on language button and open sidenav
+  // This was implimented because you can only have one language selector since google translate uses a specific ID
+  languageEl.addEventListener('click', function() {
+    // Something that's interesting is "instances" returns an array
+    instances[0].open();
+  });
+});
 
 /* Form validation */ 
 
